8e54f7b7f61fe8414314063d61470fe14787e1a3
Oziel-Silva <oziel.service@gmail.com>
2021-11-04 15:55:53 -0300

    chore: add main and variables .tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "project_id" {
+  description = "name of project_id"
+  type = string
+}
+
+variable "region" {
+  default = "us-east4"
+}
+
+variable "zone" {
+  default = "us-east4-b"
+}
